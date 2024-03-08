a3292561c3de163214cfdfb4939c73ce1b5ea0d3
HigasaOR <lee1003094395@gmail.com>
2024-03-08 08:08:14 +0800

    Initial Commit

---
+++ FoodPin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPin
+//
+//  Created by Chien Lee on 2024/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Chien Lee on 2024/3/8.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
